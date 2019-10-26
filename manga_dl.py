699c7825466a2051fe4ba8f6e1f53a1f94a9ccc7
kuriringohankamehameha <kuriringohankamehameha@github.com>
2019-10-27 02:48:39 +0530

    Change tab-width to 4

--- manga_dl.py
+++ manga_dl.py
@@ -79,103 +79,100 @@ def make_pdf_simple(pdfFileName, listPages, curr, parent):
 
 
 def download_chapter(num, chapter_url, manga_dir):
-	""" Download #num chapter of the manga """
-	if isinstance(num, int):
-		curr = manga_dir
-		new_dir = os.path.join(curr, 'chapter_' + str(num))
-		if os.path.isdir(new_dir) is False:
-			os.mkdir(new_dir)
-		os.chdir(new_dir)
-		url_list = process_chapter([], chapter_url)
-		listPages = []
-		i = 2
-		for img_url in url_list:
-			r = requests.get(img_url)
-			with open('chapter_' + str(num) + '_' +
-					  str(i) + '.jpg', 'wb') as f:
-				f.write(r.content)
-				listPages.append('chapter_' + str(num) + '_' + str(i))
-				i += 1
-		if my_system != 'W':
-			pid = os.fork()
-			if pid > 0:
-				os.chdir(curr)
-			else:
-				make_pdf_simple("chapter" + str(num), listPages, os.getcwd(), curr)
-				remove(new_dir)
-				print('Finished Chapter ' + str(num))
-				exit(0)
-		else:
-			make_pdf_simple("chapter" + str(num), listPages, os.getcwd(), curr)
-			os.chdir(curr)
-			remove(new_dir)
-			print('Finished Chapter ' + str(num))
+    """ Download #num chapter of the manga """
+    if isinstance(num, int):
+        curr = manga_dir
+        new_dir = os.path.join(curr, 'chapter_' + str(num))
+        if os.path.isdir(new_dir) is False:
+            os.mkdir(new_dir)
+        os.chdir(new_dir)
+        url_list = process_chapter([], chapter_url)
+        listPages = []
+        i = 2
+        for img_url in url_list:
+            r = requests.get(img_url)
+            with open('chapter_' + str(num) + '_' +
+                      str(i) + '.jpg', 'wb') as f:
+                f.write(r.content)
+                listPages.append('chapter_' + str(num) + '_' + str(i))
+                i += 1
+        if my_system != 'W':
+            pid = os.fork()
+            if pid > 0:
+                os.chdir(curr)
+            else:
+                make_pdf_simple("chapter" + str(num), listPages, os.getcwd(), curr)
+                remove(new_dir)
+                print('Finished Chapter ' + str(num))
+                exit(0)
+        else:
+            make_pdf_simple("chapter" + str(num), listPages, os.getcwd(), curr)
+            os.chdir(curr)
+            remove(new_dir)
+            print('Finished Chapter ' + str(num))
 
 if __name__ == '__main__':
-	"""
-	Mirror Options:
-		1. https://manganelo.com
-		2. https://mangakalot.com
-	"""
+    """
+    Mirror Options:
+            1. https://manganelo.com
+            2. https://mangakalot.com
+    """
 
-	MIRROR = 'https://manganelo.com/'
+    MIRROR = 'https://manganelo.com/'
 
-	manga_name, manga_hash = search.display_search(' '.join(sys.argv[1:]))
+    manga_name, manga_hash = search.display_search(' '.join(sys.argv[1:]))
 
-	if manga_name is None:
-		exit(0)
+    if manga_name is None:
+        exit(0)
 
-	URL_MANGA = MIRROR + 'manga/' + manga_hash
-	TITLE = manga_name
-	pattern = r'.*chapter\ [0-9]+'
+    URL_MANGA = MIRROR + 'manga/' + manga_hash
+    TITLE = manga_name
+    pattern = r'.*chapter\ [0-9]+'
 
-	chapters = []
-	chap_names = []
-	chap_list = []
+    chapters = []
+    chap_names = []
+    chap_list = []
 
-	inp_string = input('Enter the search query:\n')
-	inp_list = inp_string.split(' ')
+    inp_string = input('Enter the search query:\n')
+    inp_list = inp_string.split(' ')
 
-	if len(inp_list) == 1 and inp_list[0].isdigit():
-		chap_list = [int(inp_list[0])]
-	elif len(inp_list) == 3 and inp_list[2] == 'range' and inp_list[0].isdigit() and inp_list[1].isdigit():
-		chap_list = [i for i in range(int(inp_list[0]), int(inp_list[1]) + 1)]
-	elif all(i.isdigit() for i in inp_list):
-		chap_list = [int(i) for i in inp_list]
-	else:
-		print('Format:')
-		print('========> 1. START_CHAP END_CHAP range')
-		print('========> 2. CHAP_1 CHAP_2 CHAP_3 ... ')
-		exit(0)
+    if len(inp_list) == 1 and inp_list[0].isdigit():
+        chap_list = [int(inp_list[0])]
+    elif len(inp_list) == 3 and inp_list[2] == 'range' and inp_list[0].isdigit() and inp_list[1].isdigit():
+        chap_list = [i for i in range(int(inp_list[0]), int(inp_list[1]) + 1)]
+    elif all(i.isdigit() for i in inp_list):
+        chap_list = [int(i) for i in inp_list]
+    else:
+        print('Format:')
+        print('========> 1. START_CHAP END_CHAP range')
+        print('========> 2. CHAP_1 CHAP_2 CHAP_3 ... ')
+        exit(0)
 
-	URL_CHAPTER = MIRROR + 'chapter/' + manga_hash + '/chapter_' + inp_list[0]
-	count = None
+    URL_CHAPTER = MIRROR + 'chapter/' + manga_hash + '/chapter_' + inp_list[0]
+    count = None
 
-	orig = os.getcwd()
-	new_dir = os.path.join(orig, TITLE)
-	if not os.path.isdir(new_dir):
-		os.mkdir(new_dir)
-	os.chdir(new_dir)
-	if my_system != 'W':
-		for i in chap_list:
-			pid = os.fork()
-			if pid > 0:
-				continue
-			else:
-				download_chapter(i, MIRROR + 'chapter/' +
-												 manga_hash + '/chapter_' + str(i),
-												 new_dir)
-				exit(0)
-	else:
-		process_jobs = []
-		for i in chap_list:
-			process_jobs.append(
-					pool.apply_async(
-							download_chapter,
-							[i, MIRROR + 'chapter/' + manga_hash + '/chapter_' + str(i),
-							new_dir]
-					))
-		for job in process_jobs:
-			# A job takes a maximum time of 300 seconds
-			job.get(timeout=300)
-	os.chdir(orig)
+    orig = os.getcwd()
+    new_dir = os.path.join(orig, TITLE)
+    if not os.path.isdir(new_dir):
+        os.mkdir(new_dir)
+    os.chdir(new_dir)
+    if my_system != 'W':
+        for i in chap_list:
+            pid = os.fork()
+            if pid > 0:
+                continue
+            else:
+                download_chapter(i, MIRROR + 'chapter/' + 
+                        manga_hash + '/chapter_' + str(i), 
+                        new_dir)
+                exit(0)
+    else:
+        process_jobs = []
+        for i in chap_list:
+            process_jobs.append(pool.apply_async(download_chapter, 
+                [i, MIRROR + 'chapter/' + manga_hash + '/chapter_' + str(i),
+                    new_dir]))
+        for job in process_jobs:
+            # A job takes a maximum time of 300 seconds
+            job.get(timeout=300)
+    os.chdir(orig)
