1025f589ceefda230be17b1daa076de90a42a194
kuriringohankamehameha <kuriringohankamehameha@github.com>
2019-10-27 11:14:32 +0530

    Add cleanup option

--- merge_manga.py
+++ merge_manga.py
@@ -3,8 +3,10 @@
 from pdfrw import PdfReader, PdfWriter
 import sys
 import os
+import shutil
 
 list_merge = False
+do_cleanup = False
 
 
 def mergepdfs(titles, name):
@@ -15,10 +17,34 @@ def mergepdfs(titles, name):
     writer.write(outfn)
 
 
+def remove(path):
+    """ param <path> could either be relative or absolute. """
+    if os.path.isfile(path):
+        os.remove(path)  # remove the file
+    elif os.path.isdir(path):
+        shutil.rmtree(path)  # remove dir and all contains
+    else:
+        raise ValueError("file {} is not a file or dir.".format(path))
+
+
+def perform_cleanup_range(start_chapter, end_chapter):
+    for i in range(start_chapter, end_chapter):
+        remove(os.path.join(os.getcwd(), "chapter" + str(i) + ".pdf"))
+
+
+def perform_cleanup_list(start_index, end_index):
+    for i in sys.argv[start_index:end_index]:
+        remove(os.path.join(os.getcwd(), "chapter" + str(i) + ".pdf"))
+
+
 def exit_with_msg():
     print('Format: ')
-    print('-----> 1. python merge_manga.py START_CHAPTER END_CHAPTER PDF_NAME')
-    print('-----> 2. python merge_manga.py list CHAP_1 CHAP_2 ...... PDF_NAME')
+    print('----> 1a. python merge_manga.py START_CHAPTER END_CHAPTER PDF_NAME')
+    print('----> 1b. python merge_manga.py START_CHAPTER END_CHAPTER PDF_NAME'
+          + ' --clean')
+    print('----> 2a. python merge_manga.py list CHAP_1 CHAP_2 ..... PDF_NAME')
+    print('----> 2b. python merge_manga.py list CHAP_1 CHAP_2 ..... PDF_NAME' +
+          ' --clean')
     exit(0)
 
 
@@ -27,6 +53,9 @@ if len(sys.argv) < 4:
 if not sys.argv[1].isdigit():
     if sys.argv[1] != 'list':
         exit_with_msg()
+    if sys.argv[-1] == '--clean':
+        sys.argv.pop()
+        do_cleanup = True
     if (not sys.argv[2].isdigit()) or (sys.argv[-1].isdigit()):
         exit_with_msg()
     list_merge = True
@@ -46,6 +75,8 @@ if list_merge:
     mergepdfs(["chapter" + str(i) + ".pdf"
                for i in sys.argv[2:max_index]],
               ' '.join(sys.argv[max_index:]))
+    if do_cleanup:
+        perform_cleanup_list(2, max_index)
 else:
     assert int(sys.argv[1]) <= int(sys.argv[2])
     for i in range(int(sys.argv[1]), int(sys.argv[2]) + 1):
@@ -55,6 +86,9 @@ else:
             print('The file {} does not exist. Download it and try again'
                   .format('chapter' + str(i) + '.pdf'))
             exit(0)
+    start_chapter, end_chapter = int(sys.argv[1]), int(sys.argv[2]) + 1
     mergepdfs(["chapter" + str(i) + ".pdf"
-               for i in range(int(sys.argv[1]), int(sys.argv[2]) + 1)],
+               for i in range(start_chapter, end_chapter)],
               ' '.join(sys.argv[3:]))
+    if do_cleanup:
+        perform_cleanup_range(start_chapter, end_chapter)
